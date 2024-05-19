d8a8f0121728088377aaf1c54d6494e202d39a73
PW <156459995+mihapower@users.noreply.github.com>
2024-05-19 04:49:03 +0200

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,41 @@
+            System.out.println("Random Number: " + item);
+        for (int i = 0; i < 10; i++) {
+        int[] data = new int[10];
+
+public class RandomDataGenerator {
+    }
+    public static void main(String[] args) {
+}
+
+        }
+        for (int item : data) {
+            data[i] = (int) (Math.random() * 100) + 1;
+        }    for (let i = 0; i < 10; i++) {
+function generateRandomData() {
+    data.forEach(item => {
+
+
+function main() {
+        data.push(Math.floor(Math.random() * 100) + 1);
+    return data;
+
+    const data = generateRandomData();
+    });
+main();
+    const data = [];
+        console.log("Random Number: " + item);
+}
+}
+    }    }
+        for (int item : data) {
+            data[i] = (int) (Math.random() * 100) + 1;
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+}
+
+public class RandomDataGenerator {
+
+        }
+        int[] data = new int[10];
+        }
+            System.out.println("Random Number: " + item);
