8a821625129feb4a650927b9c431b110c0268af1
PW <156459995+mihapower@users.noreply.github.com>
2024-05-22 22:11:40 +0200

    Update code_2.py

--- code_2.py
+++ code_2.py
@@ -1,14 +1,13 @@
-    data = [random.randint(1, 100) for _ in range(10)]
-
-    data = generate_random_data()
-import random
-    main()
-    for item in data:
-
-    return data
-def generate_random_data():
 
 if __name__ == "__main__":
-
 def main():
+import random
+    data = [random.randint(1, 100) for _ in range(10)]
         print(f"Random Number: {item}")
+
+    for item in data:
+    return data
+def generate_random_data():
+    data = generate_random_data()
+    main()
+
